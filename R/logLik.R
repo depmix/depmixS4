68bf56e553dba956a85ba9a3f92ff0fd8d2167d6
Maarten Speekenbrink <4246553+mspeekenbrink@users.noreply.github.com>
2012-06-15 11:32:12 +0000

    - in fb, missing cases are now only NA, not NaN - in logLik, default method is now "fb" (for speed as this is implemented in C)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/depmix/pkg/depmixS4@519 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/logLik.R
+++ R/logLik.R
@@ -1,6 +1,7 @@
 # depends on getpars and nobs
 setMethod("logLik",signature(object="depmix"),
-	function(object,method="lystig") { 
+	#function(object,method="lystig") { 
+	function(object,method="fb") { #4/5/2012: set to fb as this is now in C
 		if(method=="fb") ll <- fb(object@init,object@trDens,object@dens,object@ntimes,object@stationary)$logLike
 		if(method=="lystig") ll <- lystig(object@init,object@trDens,object@dens,object@ntimes,object@stationary)$logLike
 		attr(ll, "df") <- freepars(object)
@@ -12,7 +13,8 @@ setMethod("logLik",signature(object="depmix"),
 
 # depends on getpars and nobs
 setMethod("logLik",signature(object="mix"),
-	function(object,method="lystig") { 
+	#function(object,method="lystig") { 
+	function(object,method="fb") { 
 		if(method=="fb") ll <- fb(object@init,matrix(0,1,1),object@dens,object@ntimes,TRUE)$logLike
 		if(method=="lystig") ll <- lystig(object@init,matrix(0,1,1),object@dens,object@ntimes,TRUE)$logLike
 		attr(ll, "df") <- freepars(object)
