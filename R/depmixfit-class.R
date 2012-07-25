ddb977fdb575a22926f00f8d3122462008f4dbef
Ingmar Visser <47030842+ingmarvisser@users.noreply.github.com>
2012-07-25 16:00:17 +0000

    Added argument compact=TRUE to show method for fitted depmix objects to give shorter presentations for models without covariates.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/depmix/pkg/depmixS4@542 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/depmixfit-class.R
+++ R/depmixfit-class.R
@@ -99,8 +99,11 @@ setMethod("show","depmix.fitted",
 	}
 )
 
+# copied from hmmr (and removed there)
+
 setMethod("summary","depmix.fitted",
-	function(object,which="all") {
+	function(object,which="all", compact=TRUE) {
+		ns <- object@nstates
 		ans=switch(which,
 			"all" = 1,
 			"response" = 2,
@@ -109,30 +112,73 @@ setMethod("summary","depmix.fitted",
 			stop("Invalid 'which' argument in summary of fitted depmix model")
 		)
 		if(ans==1|ans==3) {
-			cat("Initial state probabilties model \n")
-			show(object@prior)
-			cat("\n")
+				# show the prior models
+				cat("Initial state probabilties model \n")
+				if(compact & object@prior@formula==~1) {
+						pr <- object@prior@parameters$coefficients
+						rownames(pr) <- ""
+						colnames(pr) <- paste("St",1:ns,sep="")
+						cat(pr,"\n")
+				} else show(object@prior)
 		}
 		if(ans==1|ans==4) {
-			for(i in 1:object@nstates) {
-				cat("Transition model for state (component)", i,"\n")
-				show(object@transition[[i]])
-				cat("\n")
-			}
-			cat("\n")
+				# show the transition models
+				if(compact & object@transition[[1]]@formula==~1) {
+						cat("\nTransition matrix \n")
+						pars <- getpars(object)
+						trm <- matrix(pars[(ns+1):(ns^2+ns)],ns,ns,byr=T)
+						rownames(trm) <- paste("fromS",1:ns,sep="")
+						colnames(trm) <- paste("toS",1:ns,sep="")
+						print(trm)
+						cat("\n")
+				} else {
+						for(i in 1:ns) {
+								cat("Transition model for state (component)", i,"\n")
+								show(object@transition[[i]])
+								cat("\n")
+						}
+						cat("\n")
+				}
 		}
 		if(ans==1|ans==2) {
-			for(i in 1:object@nstates) {
-				cat("Response model(s) for state", i,"\n\n")
-				for(j in 1:object@nresp) {
-					cat("Response model for response",j,"\n")
-					show(object@response[[i]][[j]])
+				# show the response models
+			if(!compact) {
+				for(i in 1:ns) {
+					cat("Response model(s) for state", i,"\n\n")
+					for(j in 1:object@nresp) {
+						cat("Response model for response",j,"\n")
+						show(object@response[[i]][[j]])
+						cat("\n")
+					}
 					cat("\n")
 				}
-				cat("\n")
+			} else {
+				cat("Response parameters \n")
+				for(j in 1:object@nresp) {
+						cat("Resp",j, ":", object@response[[1]][[j]]@family$family, "\n")
+				}
+				pars <- list()
+				np <- numeric(object@nresp)
+				for(j in 1:object@nresp) {
+					np[j] <- npar(object@response[[1]][[j]])
+					pars[[j]] <- matrix(,nr=ns,nc=np[j])
+				}
+				allpars <- matrix(,nr=ns,nc=0)
+				nms <- c()
+				for(j in 1:object@nresp) {
+					for(i in 1:ns) {
+						pars[[j]][i,]=getpars(object@response[[i]][[j]])
+				}
+				nms <- c(nms,paste("Resp",j,1:np[j],sep="."))
+					allpars <- cbind(allpars,pars[[j]])					
+				}
+				rownames(allpars) <- paste("St",1:ns,sep="")
+				colnames(allpars) <- nms
+				print(allpars)
 			}
 		}
 	}
 )
+
 
 
